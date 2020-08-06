dfbfc3a3d42163e5a0c5ca60bef844d7975d1a04
avosa <websterb17@gmail.com>
2020-08-06 15:45:42 +0300

    Added new files

--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -1,47 +1,52 @@
-import React from 'react';
-import {ImageBackground, StyleSheet, View, Image, Text } from "react-native";
+import React from 'react'
+import {
+	ImageBackground,
+	Image,
+	View,
+	SafeAreaView,
+	StyleSheet,
+	Text
+} from 'react-native'
 
-function WelcomeScreen(props) {
-  return (
-    <ImageBackground
-    style={styles.background}
-    source={require('../assets/background.jpg')}
-    >
-      <View style={styles.logoContainer}>
-        <Image style={styles.logo} source={require("../assets/logo-red.png")} />
-        <Text>Cash in What you no longer need</Text>
-      </View>
+import Button from '../components/Button'
+import routes from '../navigation/routes'
+const WelcomeImage = require('../assets/background.jpg')
+const Logo = require('../assets/logo-red.png')
 
-      <View style={styles.loginButton}></View>
-      <View style={styles.registerButton}></View>
-    </ImageBackground>
-
-  );
-},
+export default function WelcomeScreen({ navigation }) {
+	return (
+		<ImageBackground
+			source={WelcomeImage}
+			style={styles.background}
+			blurRadius={10}>
+			<View style={styles.logoContainer}>
+				<Image source={Logo} style={styles.logo} />
+				<Text style={styles.tagline}>Sell What You Don't Need</Text>
+			</View>
+			<SafeAreaView style={styles.buttonContainer}>
+				<Button
+					title='Login'
+					onPress={() => navigation.navigate(routes.LOGIN)}
+				/>
+				<Button
+					color='secondary'
+					title='Register'
+					onPress={() => navigation.navigate(routes.REGISTER)}
+				/>
+			</SafeAreaView>
+		</ImageBackground>
+	)
+}
 
 const styles = StyleSheet.create({
-  background: {
-    flex: 1,
-    justifyContent: "flex-end",
-    alignItems: "center",
-  },
-  loginButton: {
-    width: "100%",
-    height: 70,
-    backgroundColor: "fc5c65",
-  },
-  logo: {
-    width: 100,
-    height: 100,
-  },
-  logoContainer: {
-    position: 'absolute',
-    top: 70,
-  },
-  registerButton: {
-    width: "100%",
-    height: 70,
-    backgroundColor: "4ecdc4",
-  },
-});
-export default WelcomeScreen;
+	background: { flex: 1 },
+	logoContainer: { flex: 0.4, alignItems: 'center', justifyContent: 'center' },
+	logo: { width: 100, height: 100, marginBottom: 10 },
+	tagline: { fontSize: 20, fontWeight: 'bold' },
+	buttonContainer: {
+		flex: 0.6,
+		justifyContent: 'flex-end',
+		alignItems: 'center',
+		margin: 15
+	}
+})
